1be1bdd3e227c2a3feb3e15842709e3ff2ca5ffa
Sven Stiels <52165058+5v3n-08@users.noreply.github.com>
2022-01-06 14:14:45 +0100

    chore(login): add login page (IN PROGRESS)

---
+++ pages/login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full h-full min-h-screen flex justify-center items-center">
+    <ui-card class="p-4 text-center">
+      <div class="text-2xl font-bold">Anmeldung</div>
+      <p>Bitte melde dich zuerst an um auf deine privaten Daten zuzugreifen</p>
+      <ui-input v-model="email"></ui-input>
+      {{ email }}
+    </ui-card>
+  </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({ layout: "blank" });
+</script>
+
+<script setup lang="ts">
+const email = useState("email", () => "Test");
+</script>
