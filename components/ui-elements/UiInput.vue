9deff5065373949edf001df2db9e8afa54552003
Sven Stiels <52165058+5v3n-08@users.noreply.github.com>
2022-01-07 23:34:29 +0100

    chore(UiInput): add input types (email, password, text)

--- components/ui-elements/UiInput.vue
+++ components/ui-elements/UiInput.vue
@@ -6,7 +6,7 @@
 
     <input
       class="bg-gray-200 appearance-none border-2 border-gray-900 rounded w-full py-2 px-4 text-gray-700 focus:outline-none focus:bg-gray-300 focus:!border-cyan-800"
-      type="text"
+      :type="type ?? 'text'"
       :value="modelValue"
       @input="(e) => emit('update:modelValue', (e.target as HTMLInputElement).value)"
     />
@@ -14,11 +14,13 @@
 </template>
 
 <script setup lang="ts">
+type TInputTypes = "text" | "password" | "email";
 interface IEmits {
   (event: "update:modelValue", value: string): void;
 }
 interface IProps {
   modelValue: string;
+  type?: TInputTypes;
   label?: string;
 }
 
