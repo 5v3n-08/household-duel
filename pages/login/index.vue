d8d646767913427abb34925b6a5b18e47ac6a9f1
Sven Stiels <52165058+5v3n-08@users.noreply.github.com>
2022-01-11 20:37:56 +0100

    chore(login): working on login page

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="w-full h-full min-h-screen flex justify-center items-center">
-    <v-card class="w-1/3 !p-2">
+    <v-card class="w-1/3 !p-2 opacity-100 ui-card" :class="isLoading ? 'animate-pulse' : ''">
       <v-card-title class="flex justify-center text-2xl font-bold">Anmeldung</v-card-title>
       <v-card-subtitle class="flex justify-center">Bitte melde dich zuerst an!</v-card-subtitle>
       <v-card-text>
         <div class="w-full px-4 py-2">
-          <ui-input v-model="email" class="mb-2" label="E-Mail:" type="email" />
-          <ui-input v-model="password" class="mt-2" label="Passwort:" type="password" />
+          <ui-input
+            v-model="email"
+            :disabled="isLoading"
+            class="mb-2"
+            label="E-Mail oder Benutzername:"
+            type="email"
+            :rules="[validateRequired]"
+          />
+          <ui-input
+            v-model="password"
+            class="mt-2"
+            label="Passwort:"
+            :disabled="isLoading"
+            type="password"
+            :rules="[validateRequired]"
+          />
         </div>
+        <v-alert border="start" color="error" class="ma-2"> I'm an alert with a start border and error color </v-alert>
         <div class="mt-5 flex justify-center">
-          <v-btn color="primary" @click="onLoginClick">Anmelden</v-btn>
+          <v-progress-circular v-if="isLoading" indeterminate color="primary"></v-progress-circular>
+          <v-btn v-else color="primary" @click="onLoginClick">Anmelden</v-btn>
         </div>
         <div class="mt-1 flex justify-center">
-          <button class="mt-2 small text-center font-bold hover:text-queen-blue-800">Noch keinen Account?</button>
+          <button :disabled="isLoading" class="mt-3 text-sm text-center font-bold hover:text-queen-blue-800">
+            Noch keinen Account?
+          </button>
         </div>
       </v-card-text>
     </v-card>
@@ -21,24 +39,31 @@
 
 <script lang="ts">
 import { API } from "~~/helpers/api";
+import { validateRequired } from "~~/helpers/validation";
 
 export default defineComponent({ layout: "blank" });
 </script>
 
 <script setup lang="ts">
-console.log(API(process.env.API_HOST).authentication.oauth);
 const email = useState("email", () => "");
 const password = useState("password", () => "");
-// const config = useRuntimeConfig();
+const isLoading = ref(false);
+// const reqError = ref<string | null>(null);
 
-const onLoginClick = () => {
-  // console.log(API.authentication.oauth);
-  const { pending, data, error } = useFetch(API(process.env.API_HOST).authentication.oauth, {
-    method: "POST",
-    body: { driver: "username", username: email.value, password: password.value },
+const onLoginClick = async () => {
+  isLoading.value = true;
+  const { data, error } = await useBackend(API.authentication.oauth, {
+    driver: "username",
+    username: email.value,
+    password: password.value,
   });
-
-  // console.log(data.value);
-  // console.log(error.value);
+  // if (error) console.log(error);
+  isLoading.value = false;
 };
 </script>
+
+<style scoped>
+.ui-card {
+  --v-medium-emphasis-opacity: 1;
+}
+</style>
